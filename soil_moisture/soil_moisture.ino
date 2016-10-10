eb1567b02e147cdb6fac74343eb1fdb1eb6c1957
Kiko Alario <franciscoalario@gmail.com>
2016-10-10 19:49:06 +0200

    Refactoring and Issue #4 Avoid battery drain

--- soil_moisture/soil_moisture.ino
+++ soil_moisture/soil_moisture.ino
@@ -1,21 +1,26 @@
-const int MOIS_SENS = 1;
+const int MOIS_SENS_PIN = 1;
 //const int FEED_MOIST_SENS = 3;
 const int DEFAULT_MOIST = 40;
 int MINIMUM_MOIST;
 
-const int GREEN_LED = 4;
-const int RED_LED = 5;
-const int SPEAKER = 6;
+const int GREEN_LED_PIN = 4;
+const int RED_LED_PIN = 5;
+const int SPEAKER_PIN = 6;
 
-const int BUTTON = 7;
+const int BUTTON_PIN = 7;
 
-const int LIGHT_SENS = 0;
-const int FEED_LIGHT_SENS = 2;
+const int LIGHT_SENS_PIN = 0;
+const int FEED_LIGHT_SENS_PIN = 2;
+const int ALARM_LIGHT_LEVEL = 60;
 
 const int NO_CONDITION_SET = 0;
 const int USE_DEFAULT_MOIST_CONDITION = 1;
 const int USE_CURRENT_MOIST_CONDITION = 2;
 const int TEST_CONDITION = 3;
+
+const long GREEN_COND_MEASUREMENT_DELAY = 600000;
+const long RED_COND_MEASUREMENT_DELAY = 60000;
+long measurementDelay;
 
 int condition;
 int prevCondition;
@@ -37,52 +42,10 @@ void loop() {
   }
   else{
     if(flagButton){//Button has been just released and condition action must be taken
-      if(condition == 1){//USE DEFAULT MOISTURE LEVEL AS MINIMUM LEVEL
-        Serial.print("MIN MOISTURE LEVEL SET TO DEFAULT: ");
-        Serial.println(DEFAULT_MOIST);
-        MINIMUM_MOIST = DEFAULT_MOIST;
-      }
-      else if(condition == 2){//USE CURRENT MOISTURE LEVEL AS MINIMUM LEVEL
-        Serial.print("MIN MOISTURE LEVEL SET TO: ");
-        MINIMUM_MOIST = readMoisture();
-        Serial.println(MINIMUM_MOIST);
-      }
-      else if(condition == 3){//TEST CONDITION
-        Serial.println("Current condition: TEST CONDITION");
-        condition = prevCondition;
-        doBlink(GREEN_LED, 250, 5);
-        doBlink(GREEN_LED, 1000, 1);
-        doLoudBlink(GREEN_LED, 250, 5, 2000);
-        doBlink(RED_LED, 250, 5);
-        doBlink(RED_LED, 1000, 1);
-        doLoudBlink(RED_LED, 250, 5, 2000);
-        playTone(100, 250, 0, 1);
-        playTone(3500, 800, 0, 1);
-      }
+      settleCondition();
     }
-      
-    flagButton = false;
-    if(condition == 1) Serial.println("Current condition: USE DEFAULT MOISTURE LEVEL");
-    else if(condition == 2){
-      Serial.print("Current condition: USE CUSTOM MOISTURE LEVEL: ");
-      Serial.println(MINIMUM_MOIST);
-    }
-    else Serial.println("Current condition: UNKNOWN");
-    
-    int val = readMoisture();
-    if(val >= MINIMUM_MOIST){
-      doBlink(GREEN_LED, map(val, MINIMUM_MOIST, 100, 500, 1500), 1);
-      //loopDelay = 60000;
-    }
-    else{
-      if(readLight() >= 60){
-        doLoudBlink(RED_LED, map(val, 0, MINIMUM_MOIST, 250, 500), 5-map(val, 0, MINIMUM_MOIST, 0, 4), 2000);
-      }
-      else{
-        doBlink(RED_LED, map(val, 0, MINIMUM_MOIST, 250, 500), 5-map(val, 0, MINIMUM_MOIST, 0, 4));
-      }
-      //loopDelay = 30000;
-    }
+
+    if(millis()-mlls >= measurementDelay) doMeasurement();
   }
 
   delay(loopDelay);
@@ -94,26 +57,26 @@ void initMoistureModule(){
 }
 
 void initLightModule(){
-  pinMode(FEED_LIGHT_SENS, OUTPUT);
+  pinMode(FEED_LIGHT_SENS_PIN, OUTPUT);
 }
 
 void initIndicationModule(){
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(SPEAKER, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
 }
 
 void initControlModule(){
-  pinMode(BUTTON, INPUT);
-  digitalWrite(BUTTON, HIGH);
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH);
   prevCondition = condition = USE_DEFAULT_MOIST_CONDITION;
-  mlls = 0;
-  loopDelay = 0;
+  mlls = measurementDelay = 0;
+  loopDelay = 200;
   flagButton = false;
 }
 
 int readMoisture(){
-  int value = analogRead(MOIS_SENS);
+  int value = analogRead(MOIS_SENS_PIN);
   //300 completely wet and 1024 completely dry
   value = 100-map(value, 300, 1024, 0, 100);
   Serial.print("Moisture level: ");
@@ -122,9 +85,9 @@ int readMoisture(){
 }
 
 int readLight(){
-  digitalWrite(FEED_LIGHT_SENS, HIGH);
-  int value = analogRead(LIGHT_SENS);
-  digitalWrite(FEED_LIGHT_SENS, LOW);
+  digitalWrite(FEED_LIGHT_SENS_PIN, HIGH);
+  int value = analogRead(LIGHT_SENS_PIN);
+  digitalWrite(FEED_LIGHT_SENS_PIN, LOW);
   value = map(value, 0, 1023, 0, 100);
   Serial.print("Light level: ");
   Serial.println(value);
@@ -144,7 +107,7 @@ void doBlink(int LED, int duration, int times){
 void doLoudBlink(int LED, int duration, int times, int freq){
   for(int i=0;i<times;i++){
     digitalWrite(LED, HIGH);
-    tone(SPEAKER, freq, duration);
+    tone(SPEAKER_PIN, freq, duration);
     delay(duration);
     digitalWrite(LED, LOW);
     delay(duration);
@@ -153,20 +116,19 @@ void doLoudBlink(int LED, int duration, int times, int freq){
 
 void playTone(int freq, int duration, int interval, int times){
   for(int i=0;i<times;i++){
-    tone(SPEAKER, freq, duration);
+    tone(SPEAKER_PIN, freq, duration);
     delay(interval);
   }
 }
 
 int readButton(){
-  return !digitalRead(BUTTON);
+  return !digitalRead(BUTTON_PIN);
 }
 
 void handleButton(){
   if(!flagButton){
     Serial.println("Button pressed");
-    loopDelay = 0;
-    doLoudBlink(GREEN_LED, 250, 1, 2000);
+    doLoudBlink(GREEN_LED_PIN, 250, 1, 2000);
     mlls = millis();
     prevCondition = condition;
     condition = NO_CONDITION_SET;
@@ -184,5 +146,65 @@ void handleButton(){
   else if(condition == USE_DEFAULT_MOIST_CONDITION && millis() - mlls > 7000){
     condition = TEST_CONDITION;
     playTone(3000, 250, 250, 3);
+  }
+}
+
+void settleCondition(){
+  if(condition == NO_CONDITION_SET){
+    condition = prevCondition;
+  }
+  else if(condition == USE_DEFAULT_MOIST_CONDITION){
+    Serial.print("MIN MOISTURE LEVEL SET TO DEFAULT: ");
+    Serial.println(DEFAULT_MOIST);
+    MINIMUM_MOIST = DEFAULT_MOIST;
+  }
+  else if(condition == USE_CURRENT_MOIST_CONDITION){
+    Serial.print("MIN MOISTURE LEVEL SET TO: ");
+    MINIMUM_MOIST = readMoisture();
+    Serial.println(MINIMUM_MOIST);
+  }
+  else if(condition == 3){//TEST CONDITION
+    Serial.println("Current condition: TEST CONDITION");
+    condition = prevCondition;
+    doBlink(GREEN_LED_PIN, 250, 5);
+    doBlink(GREEN_LED_PIN, 1000, 1);
+    doLoudBlink(GREEN_LED_PIN, 250, 5, 2000);
+    doBlink(RED_LED_PIN, 250, 5);
+    doBlink(RED_LED_PIN, 1000, 1);
+    doLoudBlink(RED_LED_PIN, 250, 5, 2000);
+    playTone(100, 250, 0, 1);
+    playTone(3500, 800, 0, 1);
+    delay(100);
+  }
+
+  flagButton = false;
+  mlls = measurementDelay = 0;
+  if(condition == 1) Serial.println("Current condition: USE DEFAULT MOISTURE LEVEL");
+  else if(condition == 2){
+    Serial.print("Current condition: USE CUSTOM MOISTURE LEVEL: ");
+    Serial.println(MINIMUM_MOIST);
+  }
+  else Serial.println("Current condition: UNKNOWN");
+}
+
+void doMeasurement(){
+  int val = readMoisture();
+  Serial.print("Measurement: ");
+  Serial.println(val);
+  mlls = millis();
+  if(val >= MINIMUM_MOIST){
+    doBlink(GREEN_LED_PIN, map(val, MINIMUM_MOIST, 100, 500, 1500), 1);
+    measurementDelay = GREEN_COND_MEASUREMENT_DELAY;
+    Serial.println("Delay set for green condition");
+  }
+  else{
+    if(readLight() >= ALARM_LIGHT_LEVEL){
+      doLoudBlink(RED_LED_PIN, map(val, 0, MINIMUM_MOIST, 250, 500), 5-map(val, 0, MINIMUM_MOIST, 0, 4), 2000);
+    }
+    else{
+      doBlink(RED_LED_PIN, map(val, 0, MINIMUM_MOIST, 250, 500), 5-map(val, 0, MINIMUM_MOIST, 0, 4));
+    }
+    measurementDelay = RED_COND_MEASUREMENT_DELAY;
+    Serial.println("Delay set for red condition");
   }
 }
