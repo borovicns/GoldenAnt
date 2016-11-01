6f16fafd6a16261e7ac8dfdd2af4b91fb95f247f
Kiko Alario <franciscoalario@gmail.com>
2016-11-01 21:37:42 +0100

    Handling buttons

--- DomoHedgie/src/main.cpp
+++ DomoHedgie/src/main.cpp
@@ -28,85 +28,38 @@ MenuItem mainMenu[mainMenuDimension];
 int menuIndex;
 
 /**
+BUTTONS VARIABLES
+**/
+
+#define PUSHED false
+#define NOT_PUSHED true
+#define WATCH_BUTTON true
+#define IGNORE_BUTTON false
+#define BUTTON_WAIT_INTERVAL 6000 //microseconds
+
+#define ENTER_BUTTON_PIN 18 //ISR Pin 5
+#define CANCEL_BUTTON_PIN 19 //ISR Pin 4
+#define SHUTOFF_BUTTON_PIN 20 //ISR Pin 3
+
+unsigned long previousMicros = 0;
+boolean loopPrevState = NOT_PUSHED;
+volatile boolean previousButtonState = NOT_PUSHED;
+volatile boolean debouncedButtonState = NOT_PUSHED;
+volatile boolean bounceState = false;
+
+/**
 ROTARY ENCODER VARIABLES
 **/
 
 static boolean rotating=false;
 bool rotaryFlag = false;
 
-const int rotaryPinA = 2;
-const int rotaryPinB = 3;
-const int rotaryDelay = 2;
+#define ROTARY_PIN_A 2
+#define ROTARY_PIN_B 3
+#define ROTARY_DELAY 2
 
 /**
-INIT MODULES
-**/
-
-void initMenuItems(){
-  menuIndex = 0;
-
-  mainMenu[menuIndex].id = menuIndex;
-  mainMenu[menuIndex].name = "Set min. temperature";
-  menuIndex++;
-
-  mainMenu[menuIndex].id = menuIndex;
-  mainMenu[menuIndex].name = "Set light intensity";
-  menuIndex++;
-
-  mainMenu[menuIndex].id = menuIndex;
-  mainMenu[menuIndex].name = "Set date/time";
-  menuIndex++;
-
-  mainMenu[menuIndex].id = menuIndex;
-  mainMenu[menuIndex].name = "Show heating time";
-  menuIndex++;
-
-  mainMenu[menuIndex].id = menuIndex;
-  mainMenu[menuIndex].name = "Show lighting time";
-  menuIndex++;
-
-  mainMenu[menuIndex].id = menuIndex;
-  mainMenu[menuIndex].name = "Turn off display";
-  menuIndex++;
-
-  menuIndex = 0;
-}
-
-/**
-* Enable the enconder control methods to detect the rotation of the rotary
-* encoder. This method is attached to the rotary encoder movement by the method
-* AttachInterrupt.
-* args: none
-* return: none
-*/
-void rotEncoder(){
-  rotating=true;
-}
-
-void initRotaryEncoder(){
-  pinMode(rotaryPinA, INPUT);
-  pinMode(rotaryPinB, INPUT);
-  attachInterrupt(digitalPinToInterrupt(rotaryPinA), rotEncoder, CHANGE);
-}
-
-/**
-GRAPHIC METHODS
-**/
-
-/**
-* Updates the main menu displaying the selected menu item in accordance with the
-* movement of the rotary encoder. Before and after the selected menu item are
-* also displayed the previous and next menu items.
-* args: none
-* return: none
-*/
-void updateMainMenu(){
-  //TODO update the main menu in accordance with the movement of the rotary
-  //encoder displaying the menu items.
-}
-
-/**
-CONTROL METHODS
+* ROTARY ENCODER METHODS
 **/
 
 /**
@@ -157,6 +110,36 @@ int moveMenuIndexBackward(){
 }
 
 /**
+* Enable the enconder control methods to detect the rotation of the rotary
+* encoder. This method is attached to the rotary encoder movement by the method
+* AttachInterrupt.
+* args: none
+* return: none
+*/
+void rotEncoder(){
+  rotating=true;
+}
+
+/**
+* Updates the main menu displaying the selected menu item in accordance with the
+* movement of the rotary encoder. Before and after the selected menu item are
+* also displayed the previous and next menu items.
+* args: none
+* return: none
+*/
+void updateMainMenu(){
+  //TODO update the main menu in accordance with the movement of the rotary
+  //encoder displaying the menu items.
+
+  //TEMPORARY CODE
+  Serial.println(previousMenuItem().name);
+  Serial.print("> ");
+  Serial.println(currentMenuItem().name);
+  Serial.println(nextMenuItem().name);
+  Serial.println("\n");
+}
+
+/**
 * This method handles the behaviour of the rotary encoder detecting the rotation
 * direction and calling for updating the main menu.
 * args: none
@@ -164,8 +147,8 @@ int moveMenuIndexBackward(){
 */
 void handleRotaryEncoder(){
   while(rotating){
-    delay(rotaryDelay);
-    if (digitalRead(rotaryPinB) == digitalRead(rotaryPinA)){
+    delay(ROTARY_DELAY);
+    if (digitalRead(ROTARY_PIN_B) == digitalRead(ROTARY_PIN_B)){
       if(!rotaryFlag){
         //CW
         rotaryFlag = true;
@@ -183,16 +166,145 @@ void handleRotaryEncoder(){
       }
       else rotaryFlag = false;
     }
-    rotating=false; // Reset the flag
+    rotating=false;
   }
 }
+
+/**
+* Executes what is necessary when the Enter button is pressed. It depends on
+* the state of the menu.
+* args: none
+* return: none
+*/
+void executeEnterButton(){
+  //TEMPORARY CODE
+  Serial.print("Option: ");
+  Serial.println(mainMenu[menuIndex].name);
+}
+
+/**
+* Executes what is necessary when the Cancel button is pressed. It depends on
+* the state of the menu.
+* args: none
+* return: none
+*/
+void executeCancelButton(){
+  //TODO
+}
+
+/**
+* Generic method to handle the pulsation of any button. The method receives the
+* button pin and executes the proper method.
+* args: buttonPin - The button pin which has triggered this method.
+* return: none
+*/
+void handleButton(int buttonPin){
+  if (bounceState == WATCH_BUTTON) {
+    boolean currentButtonState = digitalRead(buttonPin);
+    if (previousButtonState != currentButtonState) {
+      bounceState = IGNORE_BUTTON;
+      previousMicros = micros();
+    }
+    previousButtonState = currentButtonState;
+  }
+  if (bounceState == IGNORE_BUTTON) {
+    unsigned long currentMicros = micros();
+    if ((unsigned long)(currentMicros - previousMicros) >= BUTTON_WAIT_INTERVAL) {
+      debouncedButtonState = digitalRead(buttonPin);
+      bounceState = WATCH_BUTTON;
+
+      switch(buttonPin){
+        case ENTER_BUTTON_PIN:
+          executeEnterButton();
+          break;
+        case CANCEL_BUTTON_PIN:
+          executeCancelButton();
+          break;
+      }
+    }
+  }
+}
+
+/**
+* The ISR method for the Enter button
+* args: none
+* return: none
+*/
+void handleEnterButton() {
+  handleButton(ENTER_BUTTON_PIN);
+}
+
+/**
+* The ISR method for the Cancel button
+* args: none
+* return: none
+*/
+void handleCancelButton(){
+  handleButton(CANCEL_BUTTON_PIN);
+}
+
+/**
+INIT MODULES
+**/
+
+void initMenuItems(){
+  menuIndex = 0;
+
+  mainMenu[menuIndex].id = menuIndex;
+  mainMenu[menuIndex].name = "Set min. temperature";
+  menuIndex++;
+
+  mainMenu[menuIndex].id = menuIndex;
+  mainMenu[menuIndex].name = "Set light intensity";
+  menuIndex++;
+
+  mainMenu[menuIndex].id = menuIndex;
+  mainMenu[menuIndex].name = "Set date/time";
+  menuIndex++;
+
+  mainMenu[menuIndex].id = menuIndex;
+  mainMenu[menuIndex].name = "Show heating time";
+  menuIndex++;
+
+  mainMenu[menuIndex].id = menuIndex;
+  mainMenu[menuIndex].name = "Show lighting time";
+  menuIndex++;
+
+  mainMenu[menuIndex].id = menuIndex;
+  mainMenu[menuIndex].name = "Turn off display";
+  menuIndex++;
+
+  menuIndex = 0;
+}
+
+void initEnterButton(){
+  pinMode(ENTER_BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENTER_BUTTON_PIN), handleEnterButton, LOW);
+}
+
+void initCancelButton(){
+  pinMode(CANCEL_BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CANCEL_BUTTON_PIN), handleCancelButton, LOW);
+}
+
+void initRotaryEncoder(){
+  pinMode(ROTARY_PIN_A, INPUT);
+  pinMode(ROTARY_PIN_B, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ROTARY_PIN_A), rotEncoder, CHANGE);
+}
+
+/**
+MAIN METHODS
+**/
 
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("INIT");
 
   initMenuItems();
   initRotaryEncoder();
+  initEnterButton();
 }
 
 void loop()
